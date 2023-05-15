a208c940e238423280cc9aeef91e184f1860d441
bosirex <dennis.dancan@gmail.com>
2023-05-15 17:59:34 +0300

    Add files via upload

---
+++ task_1.c
@@ -0,0 +1,72 @@
+#include "shell.h"
+
+#define MAX_COMMAND_LENGTH 100
+
+/**
+ * display_prompt - dispays the prompt
+*/
+void display_prompt() 
+{
+    printf("simple_shell$ ");
+    fflush(stdout);
+}
+
+int main() 
+{
+    char command[MAX_COMMAND_LENGTH];
+
+    while (1) 
+    {
+        display_prompt();
+
+        // Read the user input command
+        if (fgets(command, sizeof(command), stdin) == NULL) 
+        {
+            // Handle end of file (Ctrl+D)
+            printf("\n");
+            break;
+        }
+
+        // Remove the trailing newline character
+        command[strcspn(command, "\n")] = '\0';
+
+        // Fork a child process
+        pid_t pid = fork();
+
+        if (pid < 0) 
+        {
+            // Error occurred while forking
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } 
+        else if (pid == 0) 
+        {
+            // Child process
+
+            // Execute the command
+            if (execlp(command, command, NULL) == -1) 
+            {
+                // Executable not found
+                fprintf(stderr, "./shell: No such file or directory %s\n", command);
+                exit(EXIT_FAILURE);
+            }
+
+            // This code is executed only if exec fails
+            exit(EXIT_SUCCESS);
+        } 
+        else 
+        {
+            // Parent process
+
+            // Wait for the child process to finish
+            int status;
+            if (waitpid(pid, &status, 0) == -1) 
+            {
+                perror("waitpid");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ task_4.c
@@ -0,0 +1,94 @@
+#include "shell.h"
+
+#define MAX_COMMAND_LENGTH 100
+
+/**
+ * display_prompt - dispays the prompt
+*/
+void display_prompt() 
+{
+    printf("simple_shell$ ");
+    fflush(stdout);
+}
+/**
+ * execute_command - handles the execution of the command
+*/
+int execute_command(char *command) 
+{
+    // Fork a child process
+    pid_t pid = fork();
+
+    if (pid < 0) 
+    {
+        // Error occurred while forking
+        perror("fork");
+        return -1;
+    } 
+    else if (pid == 0) 
+    {
+        // Child process
+
+        // Execute the command
+        if (execvp(command, NULL) == -1) 
+        {
+            // Executable not found
+            fprintf(stderr, "./shell: No such file or directory %s\n", command);
+            return -1;
+        }
+
+        // This code is executed only if exec fails
+        return 0;
+    } 
+    else 
+    {
+        // Parent process
+
+        // Wait for the child process to finish
+        int status;
+        if (waitpid(pid, &status, 0) == -1) 
+        {
+            perror("waitpid");
+            return -1;
+        }
+        return status;
+    }
+}
+/**
+ * main - parses the command arguments, executes the command, 
+ * and handles the end-of-file condition
+*/
+int main() 
+{
+    char command[MAX_COMMAND_LENGTH];
+
+    while (1) 
+    {
+        display_prompt();
+
+        // Read the user input command
+        if (fgets(command, sizeof(command), stdin) == NULL) 
+        {
+            // Handle end of file (Ctrl+D)
+            printf("\n");
+            break;
+        }
+
+        // Remove the trailing newline character
+        command[strcspn(command, "\n")] = '\0';
+
+        if (strcmp(command, "exit") == 0) 
+        {
+            // Exit the shell if the command is "exit"
+            break;
+        }
+
+        int status = execute_command(command);
+        if (status == -1) 
+        {
+            // An error occurred, display the prompt again
+            continue;
+        }
+    }
+
+    return 0;
+}
