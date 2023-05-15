0f5a1ce92ddd9dd2316e849ca57171ae9f80d83c
bosirex <dennis.dancan@gmail.com>
2023-05-15 02:56:52 +0000

    commit

--- shell.h
+++ shell.h
@@ -7,18 +7,4 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int sum(int a, int b);
-void remove_newline(char* str);
-void execute_command(char* command);
-char **parse_arguments(char *command);
-void execute_command(char **arguments);
-int execute_command(char *command);
-void execute_command(char *command, char *args[]);
-char* read_command();
-char* find_executable(char* command);
-int execute_command(char* command);
-
-
-
-
 #endif
