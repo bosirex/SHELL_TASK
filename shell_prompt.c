0abab0624cf18ccee54ec89a4c8a50cf2618787a
bosirex <dennis.dancan@gmail.com>
2023-05-22 13:50:23 +0300

    Add files via upload

--- shell_prompt.c
+++ shell_prompt.c
@@ -51,3 +51,9 @@ void shell_prompt(char **av, char **env)
             wait(&child_status);
     }
 }
+int main(int ac, char **av, char **env)
+{
+    if (ac == 1)
+    shell_prompt(av, env);
+    return (0);
+}
