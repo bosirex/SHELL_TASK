5d5f01f671542ffffc4a3a6a9b57d56e2e08d0eb
bosirex <dennis.dancan@gmail.com>
2023-05-22 10:22:40 +0300

    Add files via upload

--- shell_prompt_2.c
+++ shell_prompt_2.c
@@ -49,7 +49,7 @@ void shell_prompt(char **av, char **env)
         if (child_pid == 0)
         {
             if (execve(argv[0], argv, env) == -1)
-                printf("%s: No such file or directory\n, av[0]");
+                printf("%c: No such file or directory\n, av[0]");
             
         }
         else
