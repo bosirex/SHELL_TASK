c40fe53bf1cd2c625ee2b751636584ac5ac6b6b6
bosirex <dennis.dancan@gmail.com>
2023-05-26 11:46:00 +0300

    Add files via upload

---
+++ _parser.c
@@ -0,0 +1,86 @@
+#include "shell.h"
+
+/**
+ * _iscmd - determines if a file is an executable command
+ * @dat: the dat struct
+ * @path: path to the file
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int _iscmd(info_t *dat, char *path)
+{
+	struct stat st;
+
+	(void)dat;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * dup_chars - duplicates characters
+ * @pathstr: the PATH string
+ * @start: starting index
+ * @stop: stopping index
+ *
+ * Return: pointer to new buffer
+ */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
+}
+
+/**
+ * _findpath - finds this cmd in the PATH string
+ * @dat: the dat struct
+ * @pathstr: the PATH string
+ * @cmd: the cmd to find
+ *
+ * Return: full path of cmd if found or NULL
+ */
+char *_findpath(info_t *dat, char *pathstr, char *cmd)
+{
+	int n = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((string_len(cmd) > 2) && _startswith(cmd, "./"))
+	{
+		if (_iscmd(dat, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[n] || pathstr[n] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, n);
+			if (!*path)
+				string_cat(path, cmd);
+			else
+			{
+				string_cat(path, "/");
+				string_cat(path, cmd);
+			}
+			if (_iscmd(dat, path))
+				return (path);
+			if (!pathstr[n])
+				break;
+			curr_pos = n;
+		}
+		n++;
+	}
+	return (NULL);
+}
---
+++ _string1.c
@@ -0,0 +1,87 @@
+#include "shell.h"
+
+/**
+ * string_cpy - copies a string
+ * @dst: the dstination
+ * @src: the source
+ *
+ * Return: pointer to dstination
+ */
+char *string_cpy(char *dst, char *src)
+{
+	int a = 0;
+
+	if (dst == src || src == 0)
+		return (dst);
+	while (src[a])
+	{
+		dst[a] = src[a];
+		a++;
+	}
+	dst[a] = 0;
+	return (dst);
+}
+
+/**
+ * string_dup - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string
+ */
+char *string_dup(const char *str)
+{
+	int len = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		len++;
+	ret = malloc(sizeof(char) * (len + 1));
+	if (!ret)
+		return (NULL);
+	for (len++; len--;)
+		ret[len] = *--str;
+	return (ret);
+}
+
+/**
+ * _puts - prints an input string
+ * @str: the string to be printed
+ *
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	if (!str)
+		return;
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+}
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	static int a;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || a >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, a);
+		a = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[a++] = c;
+	return (1);
+}
